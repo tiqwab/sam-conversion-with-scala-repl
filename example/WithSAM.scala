951ba185414f013ce6fcb0c035851951ea7f4120
tiqwab <tiqwab.ch90@gmail.com>
2019-02-02 12:18:16 +0900

    Submit in submit

--- example/WithSAM.scala
+++ example/WithSAM.scala
@@ -4,7 +4,7 @@ object WithSAM {
 
   def main(args: Array[String]): Unit = {
     val s: java.util.concurrent.ExecutorService = java.util.concurrent.Executors.newFixedThreadPool(5)
-    s.submit(() => 1).get
+    s.submit(() => s.submit(() => 1).get).get
   }
 
 }
--- example/WithoutSAM.scala
+++ example/WithoutSAM.scala
@@ -5,7 +5,11 @@ object WithoutSAM {
   def main(args: Array[String]): Unit = {
     val s: java.util.concurrent.ExecutorService = java.util.concurrent.Executors.newFixedThreadPool(5)
     s.submit(new java.util.concurrent.Callable[Int]() {
-      def call(): Int = 1
+      def call(): Int = {
+        s.submit(new java.util.concurrent.Callable[Int]() {
+          def call(): Int = 1
+        }).get
+      }
     }).get
   }
 
